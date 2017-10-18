b99ad886d35ae163f76743cb6b75b6be050de5fa
badman2008 <abadman2008@163.com>
2017-10-18 15:46:11 +0800

    加入数据库和表的各项配置。

---
+++ app/src/main/java/com/qweather/android/db/County.java
@@ -0,0 +1,50 @@
+package com.qweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/10/18.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private int countyCode;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
